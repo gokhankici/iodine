b891d6086c41796ed116107d3380e00437c51c9f
Rami Gökhan Kıcı <gokhankici@gmail.com>
2018-02-20 11:09:39 -0800

    updated mod1.pl

--- examples/minimal/mod1.pl
+++ examples/minimal/mod1.pl
@@ -35,14 +35,6 @@ inv( A_L1,  B_L1,  C_L1,  CLK_L1
             , B_RT, C_RT
             ),
 
-        %% global prev inv holds
-        inv( A_L,  B_L,  C_L,  CLK_L
-           , A_R,  B_R,  C_R,  CLK_R
-           , A_TL, B_TL, C_TL, CLK_TL
-           , A_TR, B_TR, C_TR, CLK_TR
-           , Done_L, Done_R
-           ),
-
         (   %% set taint bits of source to 1, rest to 0
             A_TL1 = 1, B_TL1 = 0, C_TL1 = 0, CLK_TL1 = 0,
             A_TR1 = 1, B_TR1 = 0, C_TR1 = 0, CLK_TR1 = 0, 
@@ -53,49 +45,35 @@ inv( A_L1,  B_L1,  C_L1,  CLK_L1
             Done_L1 = 0, Done_R1 = 0,
             Done_L = 0, Done_R = 0
            
-        ;   (   %% run block1 on left
+        ;   (   ite(A_L = A_R, UF1_L = UF1_R, true),
+
+                %% run block1 on left
                 next1( A_L,   B_L,   UF1_L
-                     , A_TL,  B_TL,  UF1_TL
+                     , A_TL,  B_TL
                      , A_L1,  B_L1,  UF1_L1
-                     , A_TL1, B_TL1, UF1_TL1
+                     , A_TL1, B_TL1
                      ),
                 %% run block1 on right
                 next1( A_R,   B_R,   UF1_R
                      , A_TR,  B_TR,  UF1_TR
                      , A_R1,  B_R1,  UF1_R1
                      , A_TR1, B_TR1, UF1_TR1
-                     ),
-                %% uf equalities
-                (AL = AR -> UF1L = UF1R) ->
-
-                %% block1's new inv should hold
-                inv1( A_L1,  B_L1
-                    , A_R1,  B_R1
-                    , A_LT1, B_LT1
-                    , A_RT1, B_RT1
-                    )
+                     )
             ) ,
-            (   %% run block2 on left
+            (   ite(B_L = B_R, UF2_L = UF2_R, true),
+
+                %% run block2 on left
                 next2( B_L,   C_L,   UF2_L
-                     , B_TL,  C_TL,  UF2_TL
+                     , B_TL,  C_TL
                      , B_L1,  C_L1,  UF2_L1
                      , B_TL1, C_TL1, UF2_TL1
                      ),
                 %% run block2 on right
                 next2( B_R,   C_R,   UF2_R
-                     , B_TR,  C_TR,  UF2_TR
+                     , B_TR,  C_TR
                      , B_R1,  C_R1,  UF2_R1
-                     , B_TR1, C_TR1, UF2_TR1
-                     ),
-                %% uf equalities
-                (BL = BR -> UF2L = UF2R) ->
-
-                %% block2's new inv should hold
-                inv2( B_L1,  C_L1
-                    , B_R1,  C_R1
-                    , B_LT1, C_LT1
-                    , B_RT1, C_RT1
-                    )
+                     , B_TR1, C_TR1
+                     )
             )
         ).
 
